a643b06057696db37f48901ba71891ee5b706f6e
Philip Flyvholm <philipflyvholm@gmail.com>
2021-11-25 16:14:39 +0100

    Added create post page

--- Shared/forms/PostFormModel.cs
+++ Shared/forms/PostFormModel.cs
@@ -12,5 +12,9 @@ namespace ProjectBank.Shared.forms
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Content length needs to be between 5-50")]
         public string Content { get; set; }
 
+        [Required]
+        [StringLength(250, MinimumLength = 5, ErrorMessage = "Content length needs to be between 5-50")]
+        public string Tags { get; set; }
+
     }
 }
