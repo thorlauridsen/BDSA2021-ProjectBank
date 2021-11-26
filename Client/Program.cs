e4c7c14ed6f3921bf7cfd9cd65ac0626fc111e8c
PhilipFlyvholm <36300927+PhilipFlyvholm@users.noreply.github.com>
2021-11-26 14:29:19 +0100

    Front end messages (#4)

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProjectBank.Client;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
