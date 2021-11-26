5e0b09a8d8706ae3c183405593744a270374dfe3
Thor Tudal Lauridsen <thtl@itu.dk>
2021-11-26 10:52:26 +0100

    Fix

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Identity.Web;
 using ProjectBank.Core;
 using ProjectBank.Infrastructure;
 using ProjectBank.Server.Model;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,14 @@ builder.Services.AddControllersWithViews().AddJsonOptions(c =>
 });
 builder.Services.AddRazorPages();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyApp.Server", Version = "v1" });
+    c.UseInlineDefinitionsForEnums();
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<ProjectBankContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectBank")));
 builder.Services.AddScoped<IProjectBankContext, ProjectBankContext>();
@@ -40,6 +49,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
     app.UseWebAssemblyDebugging();
 }
