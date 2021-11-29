74d13c70ea536e47629bcc20caa164c4bbbc053d
Trindkr <mtot@itu.dk>
2021-11-29 11:03:19 +0100

    Add post repo tests

---
+++ Infrastructure.Tests/PostRepositoryTests.cs
@@ -0,0 +1,74 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ProjectBank.Core;
+using ProjectBank.Infrastructure;
+using Xunit;
+
+namespace Infrastructure.Tests
+{
+    public class PostRepositoryTests: IDisposable
+    {
+        private readonly ProjectBankContext _context;
+        private readonly PostRepository _repository;
+        //private readonly SupervisorRepository _supervisorRepository;
+
+        public PostRepositoryTests()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            var builder = new DbContextOptionsBuilder<ProjectBankContext>();
+            builder.UseSqlite(connection);
+            var context = new ProjectBankContext(builder.Options);
+            context.Database.EnsureCreated();
+
+
+            //Add test stuff to database here
+            //context.
+
+
+            //context.SaveChanges();
+
+            _context = context;
+            _repository = new PostRepository(_context);
+            
+
+        }
+
+        [Fact]
+        public async Task CreateAsync_creates_new_post_with_generated_id()
+        {
+            var post = new PostCreateDto
+            {
+                Title = "Biology project",
+                Content = "Bla bla bla bla",
+                SupervisorId = 1,
+                Tags = new HashSet<string> { "bio","dna","cells" }
+
+            };
+
+            var created = await _repository.CreateAsync(post);
+
+            Assert.Equal(1, created.Id);
+            Assert.Equal("Biology project", created.Title);
+            Assert.Equal("Bla bla bla bla", created.Content);
+            Assert.Equal(1, created.SupervisorId);
+            Assert.True(created.Tags.SetEquals(new[] {"bio","dna","cells"}));
+        }
+        [Fact]
+        public async Task ReadAsync_returns_all_characters()
+        {
+            var posts = await _repository.ReadAsync();
+
+            Assert.Collection(posts,
+                //character => Assert.Equal(new CharacterDto(1, "Clark", "Kent", "Superman"), character),
+               
+            );
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
