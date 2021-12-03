4caed1e772e361c1a06dc7000f248bb7f811ddb7
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2021-12-03 14:07:21 +0100

    Add code coverage package

--- Infrastructure/Post.cs
+++ Infrastructure/Post.cs
@@ -4,14 +4,15 @@ namespace ProjectBank.Infrastructure
     {
         public int Id { get; set; }
 
+        [Required]
         [StringLength(255)]
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
 
-        public string Content { get; set; }
+        public string Content { get; set; } = "";
 
         public DateTime DateAdded { get; set; }
 
-        public User User { get; set; }
+        public User User { get; set; } = null!;
 
         public ICollection<Tag> Tags { get; set; } = null!;
     }
