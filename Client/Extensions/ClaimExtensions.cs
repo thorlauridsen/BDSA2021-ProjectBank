b21ace03db043ddb3500bf043feb3897d482d75d
Philip Flyvholm <philipflyvholm@gmail.com>
2021-12-06 12:17:49 +0100

    Adds user if not exists

---
+++ Client/Extensions/ClaimExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace ProjectBank.Client.Extensions;
+
+public static class ClaimExtensions
+{
+    public static string? GetClaim(this IEnumerable<Claim> claims, string type)
+    {
+        return claims.FirstOrDefault(c => c.Type.Equals(type))?.Value;
+    }
+}
