8a92d214305ef60d0550d3c865c280e6b87fc37a
Jonas Lindvig <jonli@itu.dk>
2021-12-08 13:41:36 +0100

    working roles

---
+++ Client/CustomUserAccount.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace ProjectBank.Client
+{
+    public class CustomUserAccount : RemoteUserAccount
+    {
+        [JsonPropertyName("roles")]
+        public string[] Roles { get; set; } = new string[] {"Fail"};
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -15,11 +15,13 @@ builder.Services.AddHttpClient("ProjectBank.ServerAPI", client => client.BaseAdd
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProjectBank.ServerAPI"));
 
-builder.Services.AddMsalAuthentication(options =>
+builder.Services.AddMsalAuthentication<RemoteAuthenticationState, CustomUserAccount>(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("api://52402b00-6d09-4615-ab91-367bf3779b56/API.Access");
-});
+    options.UserOptions.RoleClaim = "appRole";
+})
+.AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, CustomUserAccount, CustomAccountFactory>();
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredModal();
