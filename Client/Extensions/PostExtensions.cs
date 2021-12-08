dd849964a9fd2392fc6405bb004e74066308f8f0
devilpant <74772780+devilpant@users.noreply.github.com>
2021-12-08 13:26:57 +0100

    Created My Projects page. However the list of projects to be displayed have not yet been successfully tested, but the code presumably functions as intended. (#38)

---
+++ Client/Extensions/PostExtensions.cs
@@ -0,0 +1,11 @@
+﻿using ProjectBank.Core;
+
+namespace ProjectBank.Client.Extensions;
+
+public static class PostExtensions
+{
+    public static IEnumerable<string> GetTagsFromPosts(this List<PostDto> posts)
+    {
+        return posts.SelectMany(p => p.Tags);
+    }
+}
